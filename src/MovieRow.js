1323f1eeb3044233b7bb7b6df88235a2a01d5a71
Gaarith Hendricks <46676957+gar18@users.noreply.github.com>
2020-09-09 19:43:37 +0200

    Update MovieRow.js

--- src/MovieRow.js
+++ src/MovieRow.js
@@ -10,17 +10,21 @@ class MovieRow extends React.Component {
 
   render() {
     return (
-      <table key={this.props.movie.id}>
+      <table class="idk" key={this.props.movie.id}>
         <tbody>
-          <tr>
-            <td>
-              <img alt="poster" width="270" src={this.props.movie.poster_src} />
-            </td>
-          </tr>
           <td>
-            <h3>{this.props.movie.title}</h3>
-            <p>{this.props.movie.overview}</p>
+            <img
+              class="poster"
+              alt="poster"
+              width="270"
+              src={this.props.movie.poster_src}
+            />
+          </td>
+          <td>
+            <h3 class="title">{this.props.movie.title}</h3>
+            <p class="overview">{this.props.movie.overview}</p>
             <input
+              class="myButton"
               type="button"
               onClick={this.viewMovie.bind(this)}
               value="View Movie"
